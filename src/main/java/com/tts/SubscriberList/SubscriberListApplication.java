e81da7bab2fc36c1e8aa4fcb7f7bf4907061d0b1
Scottie <mr.horne@gmail.com>
2021-02-05 09:20:42 -0600

    Setup Subscriber class variables, constructor, getter, setter, and toString override

--- src/main/java/com/tts/SubscriberList/SubscriberListApplication.java
+++ src/main/java/com/tts/SubscriberList/SubscriberListApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SubscriberListApplication {
-	private Long Id;
-	private String firstName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriberListApplication.class, args);
--- src/main/java/com/tts/SubscriberList/subscriber/Subscriber.java
+++ src/main/java/com/tts/SubscriberList/subscriber/Subscriber.java
@@ -1,4 +1,65 @@
 package com.tts.SubscriberList.subscriber;
 
+import java.util.Date;
+
 public class Subscriber {
+    private  Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private Date signedUp;
+
+    public Subscriber(String firstName, String lastName, String userName, Date signedUp){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.signedUp = signedUp;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getSignedUp() {
+        return signedUp;
+    }
+
+    public void setSignedUp(Date signedUp) {
+        this.signedUp = signedUp;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscriber{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", signedUp=" + signedUp +
+                '}';
+    }
 }
+
+
+
