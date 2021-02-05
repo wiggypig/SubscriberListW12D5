b6f12850fec626c11409864dc2ee772a49bf9c3b
Scottie <mr.horne@gmail.com>
2021-02-05 11:35:08 -0600

    completed Subscriber list application

--- src/main/java/com/tts/SubscriberList/subscriber/SubscriberController.java
+++ src/main/java/com/tts/SubscriberList/subscriber/SubscriberController.java
@@ -1,13 +1,32 @@
 package com.tts.SubscriberList.subscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class SubscriberController {
-    @GetMapping
+    // start the app to collect user info from index
+    @Autowired
+    private SubscriberRepository subscriberRepository;
+
+    @GetMapping(value = "/")
     public String index(Subscriber subscriber){
         // where the application gets started
         return "subscriber/index";
+    }
+
+    //  post the information to the database and display a "confirmation" on a new template called "result"
+    private Subscriber subscriber;
+    @PostMapping(value = "/")
+    public String addNewSubscriber(Subscriber subscriber, Model model) {
+        subscriberRepository.save(new Subscriber(subscriber.getFirstName(),
+                subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName());
+        return "subscriber/result";
     }
 }
---
+++ src/main/java/com/tts/SubscriberList/subscriber/SubscriberRepository.java
@@ -0,0 +1,6 @@
+package com.tts.SubscriberList.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+}
